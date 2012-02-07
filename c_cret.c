3ab1b65b7118ffe5ae09abe1b928f4d94ff6eeff
Gary Doran, Jr <garydoranjr@gmail.com>
2012-02-06 22:45:58 -0500

    Add some more comments

--- c_cret.c
+++ c_cret.c
@@ -19,7 +19,7 @@ int main() {
     // Holds file descriptors for read and write ends of the pipe
     int pipefd[2];
 
-    // Create the pipe
+    // Create the (unidirectional) pipe
     if (pipe(pipefd)) {
         perror("pipe");
         exit(EXIT_FAILURE);
@@ -43,33 +43,59 @@ int main() {
         rev_child(pipefd);
     }
 
+    // The parent needs to close its
+    // references to the pipes...
+    // (Warning: this is lazy because it doesn't
+    //           check return values of `close`!)
     close(pipefd[READ_END]);
     close(pipefd[WRITE_END]);
 
-    // The lazy way to wait...
-    // (without checking return values)
-    wait(NULL);
-    wait(NULL);
+    // Wait for children
+    int status1, status2;
+    wait(&status1);
+    wait(&status2);
 
-    return EXIT_SUCCESS;
+    // If either status = 1 (FAILURE)
+    // then the overall process failed
+    return (status1 | status2);
 }
 
 int echo_child(int *pipefd) {
+    // Close the end of the pipe this child
+    // is not going to use...
     close(pipefd[READ_END]);
+
+    // Close STDOUT so we can replace its file
+    // descriptor with that of the pipe's
+    // "write" end (TODO: error checking)
     close(STDOUT_FD);
-    dup(pipefd[WRITE_END]);
+
+    // Duplicates this file descriptor so that STDOUT
+    // also points to the write end of the pipe
+    dup2(pipefd[WRITE_END], STDOUT_FD);
 
     // This child becomes `echo $message`
+    // with STDOUT redirected to the pipe
     execlp("echo", "echo", message, (char *) NULL);
     _exit(EXIT_FAILURE);
 }
 
 int rev_child(int *pipefd) {
+    // Close the end of the pipe this child
+    // is not going to use...
     close(pipefd[WRITE_END]);
+
+    // Close STDIN so we can replace its file
+    // descriptor with that of the pipe's
+    // "read" end (TODO: error checking)
     close(STDIN_FD);
+
+    // Duplicates this file descriptor so that STDIN
+    // also points to the read end of the pipe
     dup(pipefd[READ_END]);
 
     // This child becomes `rev`
+    // with STDIN redirected from the pipe
     execlp("rev", "rev", (char *) NULL);
     _exit(EXIT_FAILURE);
 }
